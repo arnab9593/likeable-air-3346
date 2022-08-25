8ee0cf3de2e13789066cb3de98d485811bec7971
arnab9593 <arnabpal679@gmail.com>
2022-08-26 02:38:05 +0530

    release version 2

---
+++ version_2/login_signup/number.js
@@ -0,0 +1,15 @@
+document.getElementById("telephone").addEventListener("keyup", function () {
+    ;
+
+    if (document.getElementById("telephone").value.length == 10) {
+        document.getElementById("next").disabled = false;
+        document.getElementById("next").classList.add("phone");
+    }
+    else {
+        document.getElementById("next").disabled = true;
+        document.getElementById("next").classList.remove("phone");
+    }
+
+
+
+})
---
+++ version_2/scripts/bike.js
@@ -0,0 +1,45 @@
+
+let getData = async () => {
+    let url = "https://sleepy-thicket-35635.herokuapp.com/api/bikes";
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+    displayData(data);
+};
+getData();
+
+let displayData = (data) => {
+    let box = document.getElementById("h_container");
+    box.innerHTML = "";
+    data.forEach((el) => {
+        const div = document.createElement("div");
+        div.setAttribute("class", "div_h");
+
+        const image = document.createElement("img");
+        image.src = el.image;
+        image.setAttribute("class", "h_img");
+
+        const price = document.createElement("h3");
+        price.innerText = `₹ ${el.price}`;
+
+        const km = document.createElement("h5");
+        //   km.innerText = el.km;
+        km.innerText = `KM - ${el.km_driven}`;
+
+        const brand = document.createElement("h4");
+        brand.innerText = el.bikename;
+
+        const location = document.createElement("p");
+        location.innerText = el.location;
+
+        div.append(image, price, km, brand, location);
+        div.addEventListener("click", () => {
+            goToNextPage(el);
+        });
+        box.append(div);
+    });
+};
+let goToNextPage = (el) => {
+    localStorage.setItem("bike_arnab", JSON.stringify(el));
+    window.location.href = "bike.html";
+};
